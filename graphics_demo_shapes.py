87ca2bbfb1a1f55ff139ac7a397abdcbfc63e4c9
Cryptic Loop <nickstride1@gmail.com>
2018-11-09 19:29:25 +0000

    Added draw rectangle and draw circle function

--- graphics_demo_shapes.py
+++ graphics_demo_shapes.py
@@ -3,6 +3,13 @@ from graphics import *
 def drawLine(x1,y1,x2,y2):
     return Line(Point(x1,y1), Point(x2,y2))
 
+def drawRectangle(x1,y1,x2,y2):
+    return Rectangle(Point(x1,y1), Point(x2,y2))
+
+def drawCircle(x,y,r):
+    return Circle(Point(x,y),r)
+
+
 def main():
     win = GraphWin("My Window",500,500)
     win.setBackground('black')
@@ -11,8 +18,13 @@ def main():
     line.setOutline(color_rgb(0,255,255))
     line.draw(win)
 
-    
+    rect = drawRectangle(100,100,200,200)
+    rect.setOutline(color_rgb(255,0,255))
+    rect.draw(win)
 
+    cir = drawCircle(300,300,50)
+    cir.setFill(color_rgb(255,255,0))
+    cir.draw(win)
 
 
     win.getMouse()
