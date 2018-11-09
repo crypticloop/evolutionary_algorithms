feb1817d4c285e8ceaf06c71ed3e4dea78721073
Cryptic Loop <nickstride1@gmail.com>
2018-11-09 19:06:38 +0000

    draw circle

--- graphics_demo.py
+++ graphics_demo.py
@@ -2,9 +2,15 @@ from graphics import *
 
 def main():
     win = GraphWin("My Window",500,500)
+    win.setBackground('black')
+
+    pt = Point(250,250)
+    cir =  Circle(pt, 50)
+    cir.setFill('blue')
+    cir.draw(win)
 
     win.getMouse()
-    win.Close()
+    win.close()
 
 
 main()
